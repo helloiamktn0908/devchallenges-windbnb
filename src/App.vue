5577084dc141527f8049a1798c802a28f18f912b
helloiamktn <helloiamktn@gmail.com>
2022-03-16 07:47:08 +0900

    検索の側だけ実装

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <Header v-bind:msg="1234" /> -->
+  <Drawer />
 </template>
 
 <script lang="ts">
+import 'normalize.css';
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+import Drawer from './components/Drawer.vue';
 
 export default defineComponent({
   components: {
-    HelloWorld,
+    Header,
+    Drawer,
   },
 });
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800&display=swap');
+#app {
+  font-family: 'Mulish', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333333;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <h1><img alt="Vue logo" src="../images/logo.svg" /></h1>
+    <div class="header__search">
+      <span class="header__location">Helsinki, Finland</span>
+      <span class="header__guests">Add guests</span>
+      <div class="header__searchButton">
+        <span class="material-icons header__searchIcon">search</span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    msg: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 6rem;
+}
+
+h1 {
+  margin: 0;
+}
+
+.header__search {
+  height: 55px;
+  width: 297px;
+  border-radius: 16px;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+}
+
+.header__location {
+  line-height: 55px;
+  width: 138px;
+  border-right: 1px solid #f2f2f2;
+  font-size: 14px;
+}
+
+.header__guests {
+  line-height: 55px;
+  width: 106px;
+  border-right: 1px solid #f2f2f2;
+  font-size: 14px;
+  color: #bdbdbd;
+}
+
+.header__searchButton {
+  width: 53px;
+}
+
+.header__searchIcon {
+  line-height: 55px;
+  font-size: 20px;
+  color: #eb5757;
+}
+</style>
